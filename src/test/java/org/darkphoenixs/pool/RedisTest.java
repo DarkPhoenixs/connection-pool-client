379138d09d47df1528f16b8530aae301058c7dcf
Victor Zeng <Victor.Zxy@outlook.com>
2015-09-21 10:17:27 +0800

    Update

--- src/test/java/org/darkphoenixs/pool/RedisTest.java
+++ src/test/java/org/darkphoenixs/pool/RedisTest.java
@@ -9,24 +9,24 @@ public class RedisTest {
 
 	@Test
 	public void test() throws Exception {
-		
+
 		PoolConfig config = new PoolConfig();
 		config.setMaxTotal(20);
 		config.setMaxIdle(5);
 		config.setMaxWaitMillis(1000);
 		config.setTestOnBorrow(true);
-		
+
 		RedisConnectionPool pool = new RedisConnectionPool(config, "localhost", 6379);
-		
+
 		for (int i = 0; i < 1000; i++) {
-			
+
 			Jedis jedis = pool.getConnection();
-			
-			jedis.set("key", "value");
-			
+
+			jedis.set("key" + i, "value" + i);
+
 			pool.returnConnection(jedis);
 		}
-		
+
 		pool.close();
 	}
 }
