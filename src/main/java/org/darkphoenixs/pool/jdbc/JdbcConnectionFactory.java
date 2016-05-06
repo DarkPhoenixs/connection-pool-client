9507ded858a6c89822c934730127d0ecb9be8677
业余布道师 <Victor.Zxy@outlook.com>
2016-05-06 14:38:53 +0800

    change copyright

--- src/main/java/org/darkphoenixs/pool/jdbc/JdbcConnectionFactory.java
+++ src/main/java/org/darkphoenixs/pool/jdbc/JdbcConnectionFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2024 Dark Phoenixs (Open-Source Organization).
+ * Copyright 2015-2016 Dark Phoenixs (Open-Source Organization).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
