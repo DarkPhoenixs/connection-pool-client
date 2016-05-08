53d7c4cc71a4e2c3aad1787da92b81ed14393baa
业余布道师 <Victor.Zxy@outlook.com>
2016-05-08 21:21:58 +0800

    refactor PoolConfig

--- src/main/java/org/darkphoenixs/pool/PoolConfig.java
+++ src/main/java/org/darkphoenixs/pool/PoolConfig.java
@@ -34,6 +34,18 @@ public class PoolConfig extends GenericObjectPoolConfig implements Serializable 
 	/** serialVersionUID */
 	private static final long serialVersionUID = -2414567557372345057L;
 
+	/** DEFAULT_TEST_WHILE_IDLE */
+	public static final boolean DEFAULT_TEST_WHILE_IDLE = true;
+	
+	/** DEFAULT_MIN_EVICTABLE_IDLE_TIME_MILLIS */
+	public static final long  DEFAULT_MIN_EVICTABLE_IDLE_TIME_MILLIS = 60000;
+	
+	/** DEFAULT_TIME_BETWEEN_EVICTION_RUNS_MILLIS */
+	public static final long  DEFAULT_TIME_BETWEEN_EVICTION_RUNS_MILLIS = 30000;
+	
+	/** DEFAULT_NUM_TESTS_PER_EVICTION_RUN */
+	public static final int  DEFAULT_NUM_TESTS_PER_EVICTION_RUN = -1;
+
 	/**
 	 * <p>Title: PoolConfig</p>
 	 * <p>Description: 默认构造方法</p>
@@ -41,9 +53,9 @@ public class PoolConfig extends GenericObjectPoolConfig implements Serializable 
 	public PoolConfig() {
 
 	    // defaults to make your life with connection pool easier :)
-	    setTestWhileIdle(true);
-	    setMinEvictableIdleTimeMillis(60000);
-	    setTimeBetweenEvictionRunsMillis(30000);
-	    setNumTestsPerEvictionRun(-1);
+	    setTestWhileIdle(DEFAULT_TEST_WHILE_IDLE);
+	    setMinEvictableIdleTimeMillis(DEFAULT_MIN_EVICTABLE_IDLE_TIME_MILLIS);
+	    setTimeBetweenEvictionRunsMillis(DEFAULT_TIME_BETWEEN_EVICTION_RUNS_MILLIS);
+	    setNumTestsPerEvictionRun(DEFAULT_NUM_TESTS_PER_EVICTION_RUN);
 	}
 }
