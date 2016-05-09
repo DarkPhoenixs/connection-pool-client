d372526b1548cc6b58cea7bbe45f717e5b6d13fc
业余布道师 <Victor.Zxy@outlook.com>
2016-05-09 20:05:34 +0800

    fix

--- src/test/java/org/darkphoenixs/pool/redis/RedisConnectionFactoryTest.java
+++ src/test/java/org/darkphoenixs/pool/redis/RedisConnectionFactoryTest.java
@@ -1,5 +1,7 @@
 package org.darkphoenixs.pool.redis;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.util.Properties;
 
 import org.apache.commons.pool2.impl.DefaultPooledObject;
@@ -9,6 +11,30 @@ import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
 
 public class RedisConnectionFactoryTest {
+
+	static {
+
+		Thread th = new Thread(new Runnable() {
+
+			private ServerSocket serverSocket;
+
+			@Override
+			public void run() {
+
+				try {
+					serverSocket = new ServerSocket(RedisConfig.DEFAULT_PORT);
+
+					serverSocket.accept();
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		th.setDaemon(true);
+		th.start();
+	}
 
 	@Test
 	public void test() throws Exception {
@@ -38,20 +64,22 @@ public class RedisConnectionFactoryTest {
 
 		factory = new RedisConnectionFactory(RedisConfig.DEFAULT_HOST,
 				RedisConfig.DEFAULT_PORT, RedisConfig.DEFAULT_TIMEOUT,
-				RedisConfig.DEFAULT_TIMEOUT, RedisConfig.DEFAULT_PASSWORD,
-				RedisConfig.DEFAULT_DATABASE, RedisConfig.DEFAULT_CLIENTNAME);
+				RedisConfig.DEFAULT_TIMEOUT, "Test", 1, "Test");
 
 		factory.setHostAndPort(new HostAndPort(RedisConfig.DEFAULT_HOST,
 				RedisConfig.DEFAULT_PORT));
-		
+
 		try {
 			factory.makeObject();
 
 		} catch (Exception e) {
 		}
-
-		factory.activateObject(new DefaultPooledObject<Jedis>(new Jedis()));
-
+		
+		try {
+			factory.activateObject(new DefaultPooledObject<Jedis>(new Jedis()));
+		} catch (Exception e) {
+		}
+		
 		try {
 			factory.activateObject(new DefaultPooledObject<Jedis>(null));
 		} catch (Exception e) {
