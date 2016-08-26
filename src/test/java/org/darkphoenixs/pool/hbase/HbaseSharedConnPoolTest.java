ba2f936d71fec1e276b27016aea8a87538de12d1
Victor Zeng <Victor.Zxy@outlook.com>
2016-08-26 09:06:25 +0800

    fix test issue.

--- src/test/java/org/darkphoenixs/pool/hbase/HbaseSharedConnPoolTest.java
+++ src/test/java/org/darkphoenixs/pool/hbase/HbaseSharedConnPoolTest.java
@@ -38,64 +38,63 @@ public class HbaseSharedConnPoolTest {
 
         Connection conn = pool.getConnection();
 
-        if (conn == null)
-            conn = new Connection() {
-                @Override
-                public Configuration getConfiguration() {
-                    return null;
-                }
+        conn = new Connection() {
+            @Override
+            public Configuration getConfiguration() {
+                return null;
+            }
 
-                @Override
-                public Table getTable(TableName tableName) throws IOException {
-                    return null;
-                }
+            @Override
+            public Table getTable(TableName tableName) throws IOException {
+                return null;
+            }
 
-                @Override
-                public Table getTable(TableName tableName, ExecutorService pool) throws IOException {
-                    return null;
-                }
+            @Override
+            public Table getTable(TableName tableName, ExecutorService pool) throws IOException {
+                return null;
+            }
 
-                @Override
-                public BufferedMutator getBufferedMutator(TableName tableName) throws IOException {
-                    return null;
-                }
+            @Override
+            public BufferedMutator getBufferedMutator(TableName tableName) throws IOException {
+                return null;
+            }
 
-                @Override
-                public BufferedMutator getBufferedMutator(BufferedMutatorParams params) throws IOException {
-                    return null;
-                }
+            @Override
+            public BufferedMutator getBufferedMutator(BufferedMutatorParams params) throws IOException {
+                return null;
+            }
 
-                @Override
-                public RegionLocator getRegionLocator(TableName tableName) throws IOException {
-                    return null;
-                }
+            @Override
+            public RegionLocator getRegionLocator(TableName tableName) throws IOException {
+                return null;
+            }
 
-                @Override
-                public Admin getAdmin() throws IOException {
-                    return null;
-                }
+            @Override
+            public Admin getAdmin() throws IOException {
+                return null;
+            }
 
-                @Override
-                public void close() throws IOException {
+            @Override
+            public void close() throws IOException {
 
-                    throw new IOException();
-                }
+                throw new IOException();
+            }
 
-                @Override
-                public boolean isClosed() {
-                    return false;
-                }
+            @Override
+            public boolean isClosed() {
+                return false;
+            }
 
-                @Override
-                public void abort(String why, Throwable e) {
+            @Override
+            public void abort(String why, Throwable e) {
 
-                }
+            }
 
-                @Override
-                public boolean isAborted() {
-                    return false;
-                }
-            };
+            @Override
+            public boolean isAborted() {
+                return false;
+            }
+        };
 
         pool.returnConnection(conn);
 
